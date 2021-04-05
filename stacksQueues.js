12f1f01a998f76db572dcc502738ff4756ddb751
februar-y <jarmah241@gmail.com>
2021-04-05 15:17:10 -0400

    stacks

---
+++ stacksQueues.js
@@ -0,0 +1,53 @@
+/*
+
+Stacks - LIFO
+
+Use linked list
+
+*/
+
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    push(value) {
+        let newNode = new Node(val);
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            let temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+
+        this.size++;
+        return this.size;
+
+        // return ++this.size
+    }
+
+    pop() {
+        if (!this.first) return null;
+        let popped = this.first;
+
+        if (this.first === this.last) {
+            this.last = null;
+        } 
+
+        this.first = this.first.next;
+        this.size--;
+        return popped.value
+    }
+}
