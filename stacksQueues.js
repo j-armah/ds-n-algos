c50dd034e23930c4098d18309d9593a09e14b0a8
februar-y <jarmah241@gmail.com>
2021-04-05 15:27:36 -0400

    queues

--- stacksQueues.js
+++ stacksQueues.js
@@ -2,7 +2,11 @@
 
 Stacks - LIFO
 
-Use linked list
+Use linked list, pop off top push on top
+
+Queues - FIFO
+
+add/remove data
 
 */
 
@@ -49,5 +53,40 @@ class Stack {
         this.first = this.first.next;
         this.size--;
         return popped.value
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    enqueue(val) {
+        let newNode = new Node(val);
+
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+
+        return ++this.size;
+    }
+
+    dequeue () {
+        if (!this.first) return null;
+
+        let temp = this.first;
+        if(this.first === this.last) {
+            this.last = null;
+        }
+
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
     }
 }
