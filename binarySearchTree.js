de319b3217f99aa441d52f26cc98816f44743082
februar-y <jarmah241@gmail.com>
2021-04-07 12:36:31 -0400

    dfs postorder

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -104,7 +104,19 @@ class BinarySearchTree {
         return data;
     }
 
-    
+    DFSPostOrder () {
+        let data = [];
+
+        function traverse(node) {
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+            data.push(node.value); 
+        }
+        traverse(this.root);
+        return data;
+    }
+
+
 }
 
 
