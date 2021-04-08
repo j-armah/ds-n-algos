59f71729d3ebcb9f874147536c6d6f6eae448d9a
februar-y <jarmah241@gmail.com>
2021-04-08 11:21:21 -0400

    insert and bubble up

--- binaryHeap.js
+++ binaryHeap.js
@@ -22,6 +22,23 @@ right child at 2n + 2
 
 class MaxBinaryHeap {
     constructor() {
-         
+         this.values = [];
+    }
+
+    insert(value) {
+        this.values.push(element)
+        this.bubbleUp();
+    }
+    bubbleUp(){
+        let idx = this.values.length - 1;
+        const element = this.values[idx]
+        while(idx > 0) {
+            let parentIdx = Math.floor((idx - 1)/ 2); 
+            let parent = this.values[parentIdx];
+            if (element <= parent) break;
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;
+        }
     }
 }
